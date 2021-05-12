a1d6950aa15bd8920ca675e5837220dd5ff080a2
William Judd <juddwilliam13@gmail.com>
2021-05-12 11:08:43 -0500

    debugging

--- public/db.js
+++ public/db.js
@@ -1,63 +1,63 @@
-const indexedDB =
-  window.indexedDB ||
-  window.mozIndexedDB ||
-  window.webkitIndexedDB ||
-  window.msIndexedDB ||
-  window.shimIndexedDB;
+const indexedDB = window.indexedDB
+  || window.mozIndexedDB
+  || window.webkitIndexedDB
+  || window.msIndexedDB
+  || window.shimIndexedDB;
 
 let db;
 
-const request = indexedDB.open("budget", 1);
+const request = indexedDB.open('budget', 1);
 
 request.onupgradeneeded = ({ target }) => {
-  let db = target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+  const dbR = target.result;
+  dbR.createObjectStore('pending', { autoIncrement: true });
 };
 
 request.onsuccess = ({ target }) => {
   db = target.result;
-    if (navigator.onLine) {
+  // check if app is online before reading from db
+  if (navigator.onLine) {
     checkDatabase();
   }
 };
 
-request.onerror = function(event) {
-  console.log("Woops! " + event.target.errorCode);
+request.onerror = (event) => {
+  console.log(`Error: ${event.target.errorCode}`);
 };
 
 function saveRecord(record) {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
   store.add(record);
 }
 
-
 function checkDatabase() {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
   const getAll = store.getAll();
 
-  getAll.onsuccess = function() {
+  getAll.onsuccess = () => {
     if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
       })
-      .then(response => {        
-        return response.json();
-      })
-      .then(() => {
-        
-        const transaction = db.transaction(["pending"], "readwrite");
-        const store = transaction.objectStore("pending");
-        store.clear();
-      });
+        .then((response) => response.json())
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(
+            ['pending'],
+            'readwrite'
+          );
+          const store = transaction.objectStore('pending');
+          store.clear();
+        });
     }
   };
 }
 
-window.addEventListener("online", checkDatabase);
+window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,66 +1,54 @@
-const CACHE_NAME = "my-site-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
+/* eslint-disable no-restricted-globals */
 
+const CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 const urlsToCache = [
-  "/",
-  "/db.js",
-  "/index.js",
-  "/manifest.json",
-  "/styles.css",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  '/',
+  '/styles.css',
+  '/db.js',
+  '/manifest.json',
+  '/index.js',
+  '/icons/icon-512x512.png',
+  '/icons/icon-192x192.png',
 ];
 
-
-self.addEventListener("install", function(event) {
-  
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-
-self.addEventListener("fetch", function(event) {
-  
-  if (event.request.url.includes("/api/")) {
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/api/')) {
     event.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
 
-        
-        return fetch(event.request)
-          .then(response => {
-            
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-
-            return response;
-          })
-
-          
-          .catch(err => {
-            
-            return cache.match(event.request);
-          });
-      }).catch(err => console.log(err))
+              return response;
+            })
+            .catch((err) => cache.match(event.request));
+        })
+        .catch((err) => console.log(err))
     );
 
     return;
   }
 
-  
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
+    fetch(event.request).catch(() => {
+      caches.match(event.request).then((response) => {
         if (response) {
           return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          
-          return caches.match("/");
+        }
+        if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/');
         }
       });
     })
